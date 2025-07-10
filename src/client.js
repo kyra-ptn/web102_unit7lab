dbf68a7384ab423da8e4b68f7661fb492146adc1
kyra-ptn <kyra@codepath.org>
2025-07-10 13:48:49 -0400

    Move URL and API key to env variables

--- src/client.js
+++ src/client.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const URL = 'https://gxrracdgamcwkvhfjinv.supabase.co'
-const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd4cnJhY2RnYW1jd2t2aGZqaW52Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIwNzg0ODksImV4cCI6MjA2NzY1NDQ4OX0.eoguuqq4j31v1av4Hw-U1icCeixRCVP2XpGLlERS8d0'
+const URL = import.meta.env.VITE_SUPABASE_URL
+const API_KEY = import.meta.env.VITE_SUPABASE_API_KEY
 
 export const supabase = createClient(URL, API_KEY)
